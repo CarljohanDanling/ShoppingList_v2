f4410440044a9f8f8261cb228e2abd9009631fc0
Carljohan Danling <carljohan.danling@gmail.com>
2020-01-30 18:02:17 +0100

    Adding Tests for WebApi with InMemoryDatabase. Will continue soon with more tests...

---
+++ src/ShoppingListWebApiTests/Mocks/RepositoryMock.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingListWebApi.Model;
+using ShoppingListWebApi.Service;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShoppingListWebApiTests.Mocks
+{
+    public class RepositoryMock
+    {
+        public Task<List<ShoppingList>> MockShoppingListResult { get; set; }
+
+        public Task<List<ShoppingList>> GetAllShoppingListsAsync()
+            => MockShoppingListResult;
+
+        //public Task<ActionResult<ShoppingList>> GetShoppingListWithRelatedItemsAsync(int shoppingListId)
+        //{
+
+        //}
+
+        //public Task InsertShoppingListAsync(ShoppingList shoppingList)
+        //{
+
+        //}
+
+        //public Task UpdateShoppingListAsync(int shoppingListId, ShoppingList values)
+        //{
+
+        //}
+
+        //public Task DeleteShoppingListAsync(int shoppingListId)
+        //{
+
+        //}
+
+        //public Task InsertItemAsync(int shoppingListId, Item values)
+        //{
+
+        //}
+
+        //public Task UpdateItemAsync(int itemId, JsonPatchDocument<Item> item)
+        //{
+
+        //}
+
+        //public Task DeleteItemAsync(int itemId)
+        //{
+
+        //}
+
+        
+
+        
+
+        
+
+
+        
+
+
+        
+    }
+}
---
+++ src/ShoppingListWebApiTests/ShoppingListControllerTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingListWebApi.Model;
+using ShoppingListWebApi.Service;
+using Xunit;
+
+namespace ShoppingListWebApiTests
+{
+    public class ShoppingListControllerTests
+    {
+        private DbContextOptions<ShoppingListContext> ReturnDbContextOptions(string databaseString)
+        {
+            return new DbContextOptionsBuilder<ShoppingListContext>()
+                .UseInMemoryDatabase(databaseName: databaseString)
+                .Options;
+        }
+
+        [Fact]
+        public async void Should_NotReturnAnyShoppingLists()
+        {
+            var options = ReturnDbContextOptions("Should_NotReturnAnyShoppingLists");
+
+            // Arrange
+            using (var context = new ShoppingListContext(options))
+            {
+                var sut = new Repository(context);
+
+                // Act
+                var result = await sut.GetAllShoppingListsAsync();
+
+                // Assert
+                Assert.Empty(result);
+            }
+        }
+
+        [Fact]
+        public async void Should_ReturnOneShoppingList()
+        {
+            var options = ReturnDbContextOptions("Should_ReturnOneShoppingList");
+
+            // Arrange
+            using (var context = new ShoppingListContext(options))
+            {
+                await context.ShoppingList.AddAsync(new ShoppingList
+                {
+                    Name = "Test",
+                    BudgetSum = 20
+                });
+
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new ShoppingListContext(options))
+            {
+                var sut = new Repository(context);
+
+                // Act
+                var result = await sut.GetAllShoppingListsAsync();
+
+                // Assert
+                Assert.Single(result);
+            }
+        }
+
+        [Fact]
+        public async void Should_ReturnMoreThanOneShoppingList()
+        {
+            var options = ReturnDbContextOptions("Should_ReturnMoreThanOneShoppingList");
+
+            // Arrange
+            using (var context = new ShoppingListContext(options))
+            {
+                await context.ShoppingList.AddRangeAsync(
+                    new ShoppingList { Name = "Test1", BudgetSum = 10 },
+                    new ShoppingList { Name = "Test2", BudgetSum = 15 });
+                
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new ShoppingListContext(options))
+            {
+                var sut = new Repository(context);
+
+                // Act
+                var result = await sut.GetAllShoppingListsAsync();
+
+                // Assert
+                Assert.True(result.Count > 1);
+            }
+        }
+    }
+}
